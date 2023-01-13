96323035871b190df8bb5fd6f551c4e6d961aa5e
Nikolay N. Nikolaev <esfimus@gmail.com>
2023-01-13 18:12:23 +0300

    Добавлена кнопка перехода к приложению калькулятор. Добавлено всплывающее сообщение, если калькулятор не установлен.

--- app/src/main/java/com/example/runappfromanotherapp/MainActivity.java
+++ app/src/main/java/com/example/runappfromanotherapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.runappfromanotherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runCalculator();
+    }
+
+    private void runCalculator() {
+        findViewById(R.id.run_calculator).setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("calculator://launch"));
+            try {
+                startActivity(intent);
+            } catch (Exception e) {
+                Toast.makeText(this, "App is not found!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
